d15b14240319907f5594ea086cc070bd0d4056ac
Ivakin Dmitriy <ivakinda@list.ru>
2021-09-03 14:52:31 +0300

    Add lesson1 tast1

---
+++ Ivakin_Dmitriy_hw_1/1.py
@@ -0,0 +1,15 @@
+duration = int(input('Enter time in seconds: '))
+
+days = duration // 86400
+hours = (duration - 86400 * days) // 3600
+minutes = (duration - 86400 * days - hours * 3600) // 60
+seconds = duration % 60
+
+time_unit = [f'{days}d', f'{hours}h', f'{minutes}m', f'{seconds}s']
+time = f'{duration} seconds = '
+
+for idx, el in enumerate(time_unit):
+    if int(el[0]) != 0:
+        time += time_unit.copy().pop(idx) + " "
+
+print(time)
