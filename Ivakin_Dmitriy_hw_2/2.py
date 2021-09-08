09056f35bd76ba4105122e0b118af516092a8212
Ivakin Dmitriy <ivakinda@list.ru>
2021-09-08 16:25:03 +0300

    upd 2 task

--- Ivakin_Dmitriy_hw_2/2.py
+++ Ivakin_Dmitriy_hw_2/2.py
@@ -1,33 +1,30 @@
-task_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+task_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов', "+привет1друг2",
+             '-45']
+num_index = []
+# Дополняем числа до двух целочисленных разрядов
 for idx, word in enumerate(task_list):
     found_num = []
     for symbol in word:
         if symbol.isdigit() is True:
             found_num.append(symbol)
     if len(found_num) > 0:
-        task_list[idx] = task_list[idx].replace(''.join(found_num), f"{''.join(found_num):0>2}")
+        task_list[idx] = word.replace(''.join(found_num), f"{''.join(found_num):0>2}")
+        num_index.append(idx)
+for num in num_index:
+    task_list.insert(num, '"')
+    task_list.insert(num + 2, '"')
+    for idx, _ in enumerate(num_index):
+        num_index[idx] += 2
+
 print(task_list)
+print(num_index)
 
-# task_list[idx] = f'{word:0>2}'
-#             if len(word) > 1:
-#                 break
-# print(task_list)
+# Обособляем числа кавычками
 
-# word_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-# for word in word_list:
-#     found_num = []
-#     for symb in word:
-#         if symb.isdigit() is True:
-#             found_num.append(symb)
-#     if len(found_num) > 0:
-#         print("Found number {:0>2}".format(''.join(found_num)))
-
-# 49
-# 50
-# 51
-# 52
-# 53
-# 54
-# 55
-# 56
-# 57
+# cur_position = 1
+# while cur_position < len(word_list):
+#     word_list.insert(cur_position, "*")
+#     # print(cur_position)
+#     cur_position += 2
+#     # print(word_list)
+# print(word_list)
