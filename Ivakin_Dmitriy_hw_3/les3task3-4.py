1d8154328fc5b3dd149c67fccffd0a01467c3b16
Ivakin Dmitriy <ivakinda@list.ru>
2021-09-14 21:43:42 +0300

    upd 3-4
    Работает, но код надо оптимизировать

--- Ivakin_Dmitriy_hw_3/les3task3-4.py
+++ Ivakin_Dmitriy_hw_3/les3task3-4.py
@@ -1,10 +1,18 @@
 def thesaurus(*args):
     dct = {}
     for el in args:
-        dct.setdefault(el[0], []).append(el)
+        word = el.split()
+        sur = word[1][0]
+        name = word[0][0]
+        dct.setdefault(sur, {})
+    for el in args:
+        word = el.split()
+        sur = word[1][0]
+        name = word[0][0]
+        if sur in dct:
+            dct[sur].setdefault(name, []).append(el)
 
     print(dct)
-    print(args)
 
 
-thesaurus('Дима', 'Саша', 'Маша', 'Даша', 'Даша', 'Мага', 'Мойша', 'Ася')
+thesaurus("Иван Сергеев", "Ивор Саркисян", "Жанна Серова", "Анна Савельева", "Петр Алексеев", "Илья Иванов", "Дима Ивакин", "Денис Иващенко")
