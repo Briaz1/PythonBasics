64e44cf9db3fa0234154517aa9a960572c4b6796
Ivakin Dmitriy <ivakinda@list.ru>
2021-09-14 22:07:24 +0300

    complete 3-4 tast
    Оптимизировано 3-4 задание

--- Ivakin_Dmitriy_hw_3/les3task3-4.py
+++ Ivakin_Dmitriy_hw_3/les3task3-4.py
@@ -1,18 +1,14 @@
-def thesaurus(*args):
-    dct = {}
-    for el in args:
-        word = el.split()
-        sur = word[1][0]
+def thesaurus_adv(*args):
+    people_dict = {}
+    for man in args:
+        word = man.split()
+        surname = word[1][0]
         name = word[0][0]
-        dct.setdefault(sur, {})
-    for el in args:
-        word = el.split()
-        sur = word[1][0]
-        name = word[0][0]
-        if sur in dct:
-            dct[sur].setdefault(name, []).append(el)
-
-    print(dct)
+        people_dict.setdefault(surname, {})
+        if surname in people_dict:
+            people_dict[surname].setdefault(name, []).append(man)
+    return people_dict
 
 
-thesaurus("Иван Сергеев", "Ивор Саркисян", "Жанна Серова", "Анна Савельева", "Петр Алексеев", "Илья Иванов", "Дима Ивакин", "Денис Иващенко")
+print(thesaurus_adv("Иван Сергеев", "Ивор Саркисян", "Жанна Серова", "Анна Савельева", "Петр Алексеев", "Илья Иванов",
+                    "Дима Ивакин", "Денис Иващенко", "Прохор Анисимов", 'Анна Селеста'))
