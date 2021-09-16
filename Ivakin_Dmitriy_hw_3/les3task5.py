fa64e797bb005271b8d111f6fde2500622b09ced
Ivakin Dmitriy <ivakinda@list.ru>
2021-09-16 18:02:00 +0300

    Added documentation 5 task
    Добавлена документация 5-го задания.

--- Ivakin_Dmitriy_hw_3/les3task5.py
+++ Ivakin_Dmitriy_hw_3/les3task5.py
@@ -1,7 +1,13 @@
 from random import choice, shuffle
 
 
-def get_jokes(amount, repeat=True):
+def get_jokes(amount: int, repeat=True):
+    """
+    :param amount: number of jokes
+    :param repeat: Repeat words in jokes, =True(optional) - yes, =False - no.
+    :return: print list of jokes
+    """
+
     nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
     adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
     adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
@@ -9,17 +15,19 @@ def get_jokes(amount, repeat=True):
 
     if repeat is True:
         for _ in range(amount):
-            jokes_list.append(''.join(f'{choice(nouns)} {choice(adverbs)} {choice(adjectives)}'))
+            jokes_list.append(''.join(f'{choice(nouns).capitalize()} {choice(adverbs)} {choice(adjectives)}'))
 
     else:
+        """shuffles the lists."""
         shuffle(nouns)
         shuffle(adverbs)
         shuffle(adjectives)
         for num in range(amount):
+            """Stop the program when all words have been used."""
             if num + 1 > len(nouns):
                 print(f'Максимальное количество шуток: {len(jokes_list)}!')
                 break
-            jokes_list.append(''.join(f'{nouns[num]} {adverbs[num]} {adjectives[num]}'))
+            jokes_list.append(''.join(f'{nouns[num].capitalize()} {adverbs[num]} {adjectives[num]}'))
 
     print(jokes_list)
 
