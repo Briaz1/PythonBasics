e08be5e1f52b6166bd5721f00a6ce56f8835b4dc
Ivakin Dmitriy <ivakinda@list.ru>
2021-09-23 15:21:10 +0300

    3 task upd

--- Ivakin_Dmitriy_hw_4/les4task2.py
+++ Ivakin_Dmitriy_hw_4/les4task2.py
@@ -1,11 +1,11 @@
 import requests
+from datetime import datetime
 
 
 def currency_rates(char_code):
     """
     Считаю использование Decimal в данном случае лишним, так как нам нужно просто сделать вывод без математических
     операций.
-    Использование модуля datetime так же считаю лишним в данной задаче, так как дату там так же нужно просто вывести.
 
     :param char_code: official currency abbreviation
     :return: exchange rate in rubles for the current date
@@ -14,11 +14,12 @@ def currency_rates(char_code):
     response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
     currency_value = response[response.find('<Value>', response.find(char_code)) + 7:
                               response.find('</Value>', response.find(char_code))]
-    date = response[response.find('Date="') + 6:response.find('Date="') + 16]
     val_name = response[response.find('<Name>', response.find(char_code)) + 6:
                         response.find('</Name>', response.find(char_code))]
+    current_date = response[response.find('Date="') + 6:response.find('Date="') + 16]
+    current_date = datetime.strptime(current_date, '%d.%m.%Y').date()
 
-    return f'Курс {char_code} ({val_name}) на текущую дату {date}:' \
+    return f'Курс {char_code} ({val_name}) на текущую дату {current_date}:' \
            f' {float(currency_value.replace(",", ".")):.2f} рублей.'
 
 
