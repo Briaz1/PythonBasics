78afa5c630d8d647af5e2847082164c6cbd7176e
Ivakin Dmitriy <ivakinda@list.ru>
2021-09-28 04:07:56 +0300

    2 taks optimized

--- Ivakin_Dmitriy_hw_6/les6task2.py
+++ Ivakin_Dmitriy_hw_6/les6task2.py
@@ -3,9 +3,9 @@ from collections import Counter
 
 def spammers_finder(logs_file):
     with open(logs_file, 'rt') as fp:
-        spammers = dict(Counter(line.split()[0] for line in fp).most_common(5))
-        for i, ip in enumerate(spammers):
-            yield f'Топ {i + 1} по количеству запросов ip-адрес {ip}. Количество запросов:  {spammers[ip]}.'
+        spammers = Counter(line.split()[0] for line in fp).most_common(5)
+        for i, ip in enumerate(spammers, 1):
+            yield f'Топ {i} по количеству запросов ip-адрес {ip[0]}. Количество запросов: {ip[1]}.'
 
 
 for spammer in spammers_finder('nginx_logs.txt'):
