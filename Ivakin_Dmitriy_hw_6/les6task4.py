0ba4fc5a5188ec5428a10a2414a66a20275f051e
Ivakin Dmitriy <ivakinda@list.ru>
2021-09-29 18:36:03 +0300

    4 task upd

--- Ivakin_Dmitriy_hw_6/les6task4.py
+++ Ivakin_Dmitriy_hw_6/les6task4.py
@@ -4,9 +4,9 @@ from itertools import zip_longest
 def users_hobby_dict(users_file, hobby_file):
     with open(users_file, 'rt', encoding='utf-8') as users, \
          open(hobby_file, 'rt', encoding='utf-8') as hobby, \
-         open('users_hobby.csv', 'wt', encoding='utf-8') as uh:
-        name_gen = (tuple(user.replace('\n', '').split(',')) for user in users)
-        interest_gen = (interest.replace('\n', '').split(',') for interest in hobby)
+         open('users_hobby.txt', 'wt', encoding='utf-8') as uh:
+        name_gen = (user.replace('\n', '') for user in users)
+        interest_gen = (interest.replace('\n', '') for interest in hobby)
         for name, interest in zip_longest(name_gen, interest_gen):
             if name is None:
                 exit(1)
